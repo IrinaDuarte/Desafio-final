bc3ca908c40098b7c12fa04f87dfcacebddb2996
IrinaDuarte <irinamottacduarte@gmail.com>
2024-11-29 15:39:39 -0300

    adicionei o quero doar e livros doados

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,17 @@
+import S from "./doados.module.scss";
+import protagonista from "../../assets/protagonista.png";
 export default function Doados() {
   return (
-    <section>
-      <h2>Doados</h2>
+    <section className={S.boxDoados}>
+      <h2>Livros Doados</h2>
+      <section className={S.boxCard}>
+        <article>
+          <img src={protagonista} alt="" />
+          <h3>O Protagonista</h3>
+          <p>Susanne Andrade</p>
+          <p>Ficção</p>
+        </article>
+      </section>
     </section>
   );
 }
